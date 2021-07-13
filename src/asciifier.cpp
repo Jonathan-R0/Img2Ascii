b9ef04b7dc7f4b5f3f37d70a162b67874a3e3379
Jonathan-R0 <50932273+Jonathan-R0@users.noreply.github.com>
2021-07-13 15:54:19 -0300

    Optimization

--- src/asciifier.cpp
+++ src/asciifier.cpp
@@ -11,9 +11,10 @@ Asciifier::Asciifier(const char* imgpath) :
 
 void Asciifier::run() {
     SDL_Color color;
-
-    for (int i = 0; i < this->h; i += this->h/100) {
-        for (int j = 0; j < this->w; j += this->w/200) {
+    auto dx = this->h/100;
+    auto dy = this->w/200;
+    for (int i = 0; i < this->h; i += dx) {
+        for (int j = 0; j < this->w; j += dy) {
             color = surface.getPixelColor(j,i);
             // 0 <= brightness <= 255
             float b = 0.299 * color.r + 0.587 * color.g + 0.114 * color.b;
